da127395e4da51f9d440f690f9ad35a795ceec4d
jiban tajpuriya <jibantajpuriya03@gmail.com>
2026-01-11 19:54:53 +0545

    updsate config file

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+     base: '/Todo-website/',
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
   ],
-  base: '/Todo-website/',
-  
 })
