e545b11dfad530020d7b178c77ad23de0d830825
jiban tajpuriya <jibantajpuriya03@gmail.com>
2026-01-11 22:27:38 +0545

    update enter press key

--- src/component/Inputfield.jsx
+++ src/component/Inputfield.jsx
@@ -1,22 +1,33 @@
-import React, { useContext, useState } from 'react'
-import './Inputfield.css'
-import { datasontex } from '../store/Datastore'
+import React, { useContext, useState } from "react";
+import "./Inputfield.css";
+import { datasontex } from "../store/Datastore";
 
 const Inputfield = () => {
- const {addData} = useContext(datasontex);
- const [itemName, setItemName] = useState("");
+  const { addData } = useContext(datasontex);
+  const [itemName, setItemName] = useState("");
+  const inpthandler = (e) => {
+    setItemName(e.target.value);
+    if (e.key === "Enter") {
+     addData(itemName, setItemName);
+    }
+  };
 
   return (
     <center>
-    <div className='inputfield-main'>
-      <div className="inputfield-area">
-          <input value={itemName} onChange={(e)=>{setItemName(e.target.value)}} placeholder='enter your todo here' required/>
-      <button onClick={()=>addData(itemName,setItemName)}> Add</button>
+      <div className="inputfield-main">
+        <div className="inputfield-area">
+          <input
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+            onKeyDown={inpthandler}
+            placeholder="enter your todo here"
+            required
+          />
+          <button onClick={() => addData(itemName, setItemName)}> Add</button>
+        </div>
       </div>
-    
-    </div>
     </center>
-  )
-}
+  );
+};
 
-export default Inputfield
+export default Inputfield;
